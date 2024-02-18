60d0eb0d216be86907f1d8336f4e57c5f28fed30
Harvinder Yadav <harvinderyadav_18528@aitpune.edu.in>
2024-02-18 22:03:47 +0530

    still in dev
    Backend

---
+++ mern-eccomerce-backend/dist/controllers/user.js
@@ -0,0 +1,57 @@
+import { User } from "../models/user.js";
+import { TryCatch } from "../middlewares/error.js";
+import ErrorHandler from "../utils/utility-class.js";
+// import {TryCatch} from "../utils/utility-class.js";
+export const newUser = TryCatch(async (req, res, next) => {
+    const { name, email, photo, gender, _id } = req.body;
+    console.log("in")
+    let user = await User.findById(_id);
+    if (user)
+        return res.status(200).json({
+            success: true,
+            message: `Welcome, ${user.name}`,
+        });
+    if (!name || !email || !photo || !gender)
+        return next(new ErrorHandler("Please add all fields", 400));
+    user = await User.create({
+        name,
+        email,
+        photo,
+        gender
+    });
+    return res.status(201).json({
+        success: true,
+        message: `Welcome, ${user.name}`,
+    });
+});
+// for admin only, find the list of all users
+export const getAllUsers = TryCatch(async (req, res, next) => {
+    const users = await User.find({});
+    return res.status(200).json({
+        success: true,
+        users,
+    });
+});
+//Search by Id.
+export const getUser = TryCatch(async (req, res, next) => {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (!user)
+        return next(new ErrorHandler("Invalid Id", 400));
+    return res.status(200).json({
+        success: true,
+        user,
+    });
+});
+//Delete user.
+export const deleteUser = TryCatch(async (req, res, next) => {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (!user)
+        return next(new ErrorHandler("Invalid Id", 400));
+    await user.deleteOne();
+    return res.status(200).json({
+        success: true,
+        message: "User Deleted Successfully",
+    });
+});
---
+++ mern-eccomerce-backend/src/routes/user.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+    deleteUser,
+    getAllUsers,
+    getUser,
+    newUser,
+  } from "../controllers/user.js";
+  import { adminOnly } from "../middlewares/auth.js";
+
+const app = express.Router();
+console.log("stage1")
+app.post("/new", newUser);  
+//user/new  -> user will be added as prefix.
+app.get("/all", adminOnly, getAllUsers);
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
+
+export default app;
